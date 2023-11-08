c774233eda3618006fc4b5829e48f7ea754154fb
SebastienRancoeur <97077046+SebastienRancoeur@users.noreply.github.com>
2023-11-08 17:01:32 -0500

    Separated into internal and boats packages

---
+++ boattracker/src/main/java/boattracker/internal/Employee.java
@@ -0,0 +1,30 @@
+package boattracker;
+
+import boattracker.boats.*;
+
+public class Employee {
+    private int empId;
+    private String department;
+    private String name;
+
+    public Employee(int employeeId, String department) {
+        this.empId = employeeId;
+        this.department = department;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDepartment() {
+        return this.department;
+    }
+
+    public void addBoat(Boat boat){
+        this.boats.add(boat);
+    }
+}
