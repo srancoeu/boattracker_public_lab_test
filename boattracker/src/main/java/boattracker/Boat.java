0f210a27d94766bf949fa1086033e6de20a2a822
SebastienRancoeur <97077046+SebastienRancoeur@users.noreply.github.com>
2023-11-08 16:15:44 -0500

    Boats now support multiple employees

--- boattracker/src/main/java/boattracker/Boat.java
+++ boattracker/src/main/java/boattracker/Boat.java
@@ -4,7 +4,8 @@ import java.util.ArrayList;
 public class Boat {
     private String modelNumber;
     private String boatColor;
-    private double boatPri
+    private double boatPrice = 0.0;
+    private ArrayList<Employee> users = new ArrayList<Employee>();
 
      
     public Boat(String modelNumber){
@@ -17,14 +18,14 @@ public class Boat {
  
     public double getBoatPrice() {
      
-
+    }
      
     public void addUser(Employee employee){
-	this.users.add(employee);
+	    this.users.add(employee);
     }
 
     public void removeUser(Employee employee){
-	this.users.remove(employee);
+	    this.users.remove(employee);
     }
 
 }
